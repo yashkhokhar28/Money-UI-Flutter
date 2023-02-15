6fdaf6da4292b19b1509a8b06bc9ed165a906d08
Yash Khokhar <yashkhokhar28@gmail.com>
2023-02-15 20:26:39 +0530

    project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:moneyui/homepage.dart';
-import 'package:moneyui/firstpage.dart';
-import 'package:moneyui/secondpage.dart';
 void main() {
   runApp(
     MaterialApp(
